cbc5cd14aa44e3fead66f4c3826562cdd7a024f6
Nick Brown <nickbrown714@gmail.com>
2016-10-10 17:04:42 -0700

    Revert "User Profiles Progress"

--- app/models/instrument_profile.rb
+++
@@ -1,4 +0,0 @@
-class InstrumentProfile < ApplicationRecord
-    belongs_to :instrument
-    belongs_to :profile
-end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,22 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-  def setup
-    @user1 = User.new(email: "user@example.com", password: "123456")
-    @user2 = User.new(email: "user@example.com", password: "12345")
-    @user3= User.new(email: "usexample", password: "123456")
-  end
-
-  test "valid user" do
-    assert @user1.valid?
-  end
-  
-  test "password too short" do
-    assert_not @user2.valid?
-  end
-  
-  test "invalid email" do
-    assert_not @user3.valid?
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
