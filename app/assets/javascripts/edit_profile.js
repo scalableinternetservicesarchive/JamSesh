083b99fd92c2991ae0da69bfe48226829d563155
John Mangel <jmangel@umail.ucsb.edu>
2016-10-17 19:43:57 +0000

    Add capability to edit instrument proficiencies

--- app/assets/javascripts/edit_profile.js
+++ app/assets/javascripts/edit_profile.js
@@ -5,6 +5,33 @@ $(document).ready(function(){
     var proficiency = $("[name='proficiency']")[0].value;
     addInstrument(instrument_id, proficiency);
   });
+  
+  $(document).on('click', '.edit_instrument', function(event) {
+    var instrument_id = event.target.id.split('-').pop();
+    var proficiency = $('#i_id-' + instrument_id + ' .proficiency .current-proficiency')[0].innerText;
+
+    $('#i_id-' + instrument_id + ' .proficiency').html(
+      '<select id="proficiency-i_id-' 
+      + instrument_id 
+      + '" class="form-control">'
+      + '<option value="Newb">Newb</option>'
+      + '<option value="Beginner">Beginner</option>'
+      + '<option value="Novice">Novice</option>'
+      + '<option value="Intermediate">Intermediate</option>'
+      + '<option value="Experienced">Experienced</option>'
+      + '<option value="Expert">Expert</option>'
+      + '<option value="Virtuoso">Virtuoso</option>'
+      + '</select>'
+      + '<button type="button" id="update-i_id-' + instrument_id + '" class="btn btn-primary update_instrument">Update</button>'
+    );
+    $('#proficiency-i_id-' + instrument_id)[0].value = proficiency;
+  });
+  
+  $(document).on('click', '.update_instrument', function(event) {
+    var instrument_id = event.target.id.split('-').pop();
+    var proficiency =$('#proficiency-i_id-' + instrument_id)[0].value;
+    addInstrument(instrument_id, proficiency);
+  });
 
 })
 
@@ -18,10 +45,19 @@ function addInstrument(instrument_id, proficiency) {
       var instrument_row = $('#i_id-' + instrument_id);
 
       if (instrument_row.length) {
-        $('#i_id-' + instrument_id + ' .proficiency').html(proficiency);
+        $('#i_id-' + instrument_id + ' .proficiency').html(
+          '<span class="current-proficiency">' + proficiency + '</span>' 
+          + '<button type="button" id="edit-i_id-' + instrument_id + '" class="btn btn-primary edit_instrument">Edit</button>'
+        );
       }
       else {
-        $('#instrument-table tbody').append('<tr><td>' + data.name + '</td><td>' + proficiency + '</td></tr>');
+        $('#instrument-table tbody').append(
+          '<tr id="i_id-' + instrument_id + '">'
+          + '<td class="table-content instrument">' + data.name + '</td>'
+          + '<td class="table-content proficiency">' 
+          + '<span class="current-proficiency">' + proficiency + '</span>'
+          + '<button type="button" id="edit-i_id-' + instrument_id + '" class="btn btn-primary edit_instrument">Edit</button>'
+          + '</td></tr>');
       }
     }
   });
