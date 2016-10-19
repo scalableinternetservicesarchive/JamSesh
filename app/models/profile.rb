16031f975dec47ea3b455b4a313a31d0b8767a36
Nick Brown <nickbrown714@gmail.com>
2016-10-18 23:36:46 -0700

    cascade deletes to avoid null pointer problems

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
-    has_many :instrument_profile
+    has_many :instrument_profile, :dependent => :destroy
     has_many :instrument, through: :instrument_profile
-    has_many :jam_group_member
+    has_many :jam_group_member, :dependent => :destroy
     has_many :jam_groups, through: :jam_group_member
     belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  has_one :profile
+  has_one :profile, :dependent => :destroy
   before_create :build_default_profile
   
   
