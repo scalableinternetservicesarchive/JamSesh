1eba05cf51cb79612138d48bb6dd9d16d3c6a4a9
Shayan <shayani.ms@gmail.com>
2016-10-20 07:49:08 +0000

    Added support for age range. Age range and instruments are now drop down options.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def home
     @profiles = Profile.all
-    @profiles = @profiles.by_age(params[:age]) if params[:age].present?
+    @profiles = @profiles.by_age_range(params[:age_low], params[:age_high]) if params[:age_low].present? or params[:age_high].present?
     @profiles = @profiles.by_location(params[:location]) if params[:location].present?
     @profiles = @profiles.by_instrument(params[:instrument]) if params[:instrument].present?
     render "home"
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   private
 
   def application_params
-    params.require(:application).permit(:age, :location, :instrument)
+    params.require(:application).permit(:age_low, :age_high, :location, :instrument)
   end
   
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ApplicationRecord
     
-    scope :by_age, lambda { |age| where(:age => age)}
+    scope :by_age_range, lambda { |age_low, age_high|   age_low = '18' if age_low == ''
+                                                        age_high = '999' if age_high == ''
+                                                        where(:age => age_low...age_high) }
     scope :by_location, lambda { |location| where(Profile.arel_table[:location].lower.matches("%#{location.downcase}%"))}
     scope :by_instrument, lambda { |instrument| self.joins(:instrument_profile, :instrument).where(Instrument.arel_table[:name].lower.matches("%#{instrument.downcase}%")).distinct }
     
