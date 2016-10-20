e813c30bd4e8107866414e17095c0e3614583213
Shayan <shayani.ms@gmail.com>
2016-10-20 08:33:23 +0000

    Added search by name.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def home
     @profiles = Profile.all
+    @profiles = @profiles.by_name(params[:name]) if params[:name].present?
     @profiles = @profiles.by_age_range(params[:age_low], params[:age_high]) if params[:age_low].present? or params[:age_high].present?
     @profiles = @profiles.by_location(params[:location]) if params[:location].present?
     @profiles = @profiles.by_instrument(params[:instrument]) if params[:instrument].present?
@@ -15,7 +16,7 @@ class ApplicationController < ActionController::Base
   private
 
   def application_params
-    params.require(:application).permit(:age_low, :age_high, :location, :instrument)
+    params.require(:application).permit(:name, :age_low, :age_high, :location, :instrument)
   end
   
 
