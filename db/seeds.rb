a7c99b5f0a748b23a4a63660a3c8908762a75e65
Nick Brown <nickbrown714@gmail.com>
2016-11-16 17:58:57 -0800

    seedier for postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,35 @@ instrument_ids = Instrument.all.map(&:id)
 jam_group_ids = JamGroup.all.map(&:id)
 
 proficiencies = [:Newb, :Beginner, :Novice, :Intermediate, :Experienced, :Expert, :Virtuoso]
-statuses = [:pending, :joined]
+statuses = [0, 1]
 
-1000.times do |n|
-  User.connection.execute "INSERT INTO users (`id`, `email`, `encrypted_password`, `created_at`, `updated_at`) VALUES (#{n}, 'person#{n}@example.com', '#{User.new(:password => "password").encrypted_password}', NOW(), NOW())"
+User.transaction do
+  1000.times do |n|
+    User.connection.execute "INSERT INTO users (id, email, encrypted_password, created_at, updated_at) VALUES (#{n}, 'person#{n}@example.com', '#{User.new(:password => "password").encrypted_password}', NOW(), NOW())"
+  end
 end
 
-1000.times do |n|
-  Profile.connection.execute "INSERT INTO profiles (`id`, `created_at`, `updated_at`, `user_id`, `first_name`, `last_name`, `age`, `location`, `bio`) VALUES (#{n}, NOW(), NOW(), #{n}, 'First#{n}', 'Last#{n}', #{(1..120).to_a.sample}, 'Santa Barbara, CA', 'Bio #{n}')" 
+Profile.transaction do
+  1000.times do |n|
+    Profile.connection.execute "INSERT INTO profiles (id, created_at, updated_at, user_id, first_name, last_name, age, location, bio) VALUES (#{n}, NOW(), NOW(), #{n}, 'First#{n}', 'Last#{n}', #{(1..120).to_a.sample}, 'Santa Barbara, CA', 'Bio #{n}')" 
+  end
 end
 
 profile_ids = Profile.all.map(&:id)
 
-1000.times do |n|
-  instrument_ids.sample((1..5).to_a.sample).each do |i|
-    Instrument.connection.execute "INSERT INTO instrument_profiles (`profile_id`, `instrument_id`, `proficiency`, `owned`, `created_at`, `updated_at`) VALUES (#{n}, #{i}, '#{proficiencies.sample}', '#{[true, false].sample}', NOW(), NOW())" 
-  end
-
-  jam_group_ids.sample((2..10).to_a.sample).each do |j|
-    JamGroupMember.connection.execute "INSERT INTO jam_group_members (`profile_id`, `jam_group_id`, `invited_by`, `status`, `created_at`, `updated_at`) VALUES (#{n}, #{j}, #{profile_ids.sample}, '#{statuses.sample}', NOW(), NOW())"
-
-    ((10..50).to_a.sample).times do |k|
-      Comment.connection.execute "INSERT INTO comments (`jam_group_id`, `profile_id`, `comment`, `created_at`, `updated_at`) VALUES (#{j}, #{n}, 'Comment #{k} from user #{n}', NOW(), NOW())"
+ActiveRecord::Base.transaction do
+  1000.times do |n|
+    instrument_ids.sample((1..5).to_a.sample).each do |i|
+      Instrument.connection.execute "INSERT INTO instrument_profiles (profile_id, instrument_id, proficiency, owned, created_at, updated_at) VALUES (#{n}, #{i}, '#{proficiencies.sample}', '#{[true, false].sample}', NOW(), NOW())" 
     end
+
+    jam_group_ids.sample((2..10).to_a.sample).each do |j|
+      JamGroupMember.connection.execute "INSERT INTO jam_group_members (profile_id, jam_group_id, invited_by, status, created_at, updated_at) VALUES (#{n}, #{j}, #{profile_ids.sample}, '#{statuses.sample}', NOW(), NOW())"
+
+      ((10..50).to_a.sample).times do |k|
+        Comment.connection.execute "INSERT INTO comments (jam_group_id, profile_id, comment, created_at, updated_at) VALUES (#{j}, #{n}, 'Comment #{k} from user #{n}', NOW(), NOW())"
+      end
+    end
+
   end
-
 end
-
