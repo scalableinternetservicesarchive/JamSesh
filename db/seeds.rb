7c332bdf47066135eebcc8a3f736db32bb836cbf
Nick Brown <nickbrown714@gmail.com>
2016-11-16 17:17:28 -0800

    better seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,18 @@ FactoryGirl.create_list(:jam_group, 200)
 proficiencies = [:Newb, :Beginner, :Novice, :Intermediate, :Experienced, :Expert, :Virtuoso]
 statuses = [:pending, :joined]
 
+users = []
+profiles = []
 1000.times do |n|
-  u = User.create(email: "person#{n+1}@example.com", password: "password", password_confirmation: "password")
+  User.connection.execute "INSERT INTO users (`id`, `email`, `encrypted_password`, `created_at`, `updated_at`) VALUES (#{n}, 'person#{n}@example.com', '#{User.new(:password => "password").encrypted_password}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)"
+end
 
-  p = u.profile
-  p.first_name = "First#{n}"
-  p.last_name = "Last#{n}"
-  p.age = (1..120).to_a.sample
-  p.location = "Santa Barbara, CA"
-  p.bio = "Bio #{n}"
-  p.save
+1000.times do |n|
+  Profile.connection.execute "INSERT INTO profiles (`id`, `created_at`, `updated_at`, `user_id`, `first_name`, `last_name`, `age`, `location`, `bio`) VALUES (#{n}, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, #{n}, 'First#{n}', 'Last#{n}', #{(1..120).to_a.sample}, 'Santa Barbara, CA', 'Bio #{n}')" 
+end
 
+1000.times do |n|
+  p = Profile.find(n)
   Instrument.limit((1..5).to_a.sample).order("RANDOM()").each do |i|
     ip = InstrumentProfile.new
     ip.profile = p
@@ -45,3 +46,4 @@ statuses = [:pending, :joined]
   end
 
 end
+
