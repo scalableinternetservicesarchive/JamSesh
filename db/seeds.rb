3d3bc34bbd148a7e422441280e64147fb2ba007e
John Mangel <jmangel@umail.ucsb.edu>
2016-12-01 10:43:35 -0800

    Add improved seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,46 +2,92 @@ require "factory_girl"
 #FactoryGirl.find_definitions
 
 FactoryGirl.create_list(:instrument, 30)
+puts "added 30 instruments to the database"
 FactoryGirl.create_list(:jam_group, 200)
+puts "added 200 jam groups to the database"
+instrument_ids = Instrument.all.map(&:id)
+jam_group_ids = JamGroup.all.map(&:id)
 
 proficiencies = [:Newb, :Beginner, :Novice, :Intermediate, :Experienced, :Expert, :Virtuoso]
-statuses = [:pending, :joined]
+statuses = [0, 1]
+password = User.new(:password => "password").encrypted_password
+letters = "abcdefghijkl"
+
+users = []
+profiles = []
+instrument_profiles = []
+members = []
+comments = []
+artists = []
+artists_fields = []
+artists_profiles = []
+
+letters.each_char {|c|
+	artist_arr =  Spotty.autocomplete(c)
+	artist_arr.each {|artist|
+		name = artist.name
+		id = artist.id
+		genres = artist.genres.map{|a| a}.join(",")
+		photo_url = artist.images.first['url']
+		artists_fields.push([name, id, photo_url, genres])
+	}
+}
+artists_fields = artists_fields.uniq
+
+for i in 0..artists_fields.length-1
+	artists << "(#{i}, '#{artists_fields[i][0]}', NOW(), NOW(), '#{artists_fields[i][1]}', '#{artists_fields[i][2]}', '#{artists_fields[i][3]}')"
+end
 
 1000.times do |n|
-  u = User.create(email: "person#{n}@example.com", password: "password", password_confirmation: "password")
-
-  p = u.profile
-  p.first_name = "First#{n}"
-  p.last_name = "Last#{n}"
-  p.age = (1..120).to_a.sample
-  p.location = "Santa Barbara, CA"
-  p.bio = "Bio #{n}"
-  p.save
-
-  Instrument.limit((1..5).to_a.sample).order("RANDOM()").each do |i|
-    ip = InstrumentProfile.new
-    ip.profile = p
-    ip.instrument = i
-    ip.proficiency = proficiencies.sample
-    ip.owned = [true, false].sample
-    ip.save
+  users << "(#{n}, 'person#{n}@example.com', '#{password}', NOW(), NOW())"
+  profiles << "(#{n}, NOW(), NOW(), #{n}, 'First#{n}', 'Last#{n}', #{(1..120).to_a.sample}, 'Santa Barbara, CA', 'Bio #{n}')" 
+  (0..artists_fields.length-1).to_a.sample(5).each do |j|
+    artists_profiles << "(#{n}, #{j})"
   end
-
-  JamGroup.limit((2..10).to_a.sample).order("RANDOM()").each do |j|
-    jgm = JamGroupMember.new
-    jgm.profile = p
-    jgm.jam_group = j
-    jgm.invited_by = Profile.offset(rand(Profile.count)).first
-    jgm.status = statuses.sample
-    jgm.save
+  instrument_ids.sample((1..5).to_a.sample).each do |i|
+    instrument_profiles << "(#{n}, #{i}, '#{proficiencies.sample}', '#{[true, false].sample}', NOW(), NOW())" 
+  end
+  jam_group_ids.sample((2..10).to_a.sample).each do |j|
+    members << "(#{n}, #{j}, #{n}, '#{statuses.sample}', NOW(), NOW())"
 
     ((10..50).to_a.sample).times do |k|
-      c = Comment.new
-      c.jam_group = j
-      c.profile = p
-      c.comment = "Comment #{k} from #{p.full_name}"
-      c.save
+      comments << "(#{j}, #{n}, 'Comment #{k} from user #{n}', NOW(), NOW())"
     end
   end
-
 end
+
+User.transaction do
+  User.connection.execute "INSERT INTO users (id, email, encrypted_password, created_at, updated_at) VALUES #{users.join(', ')}"
+  puts "added #{users.count} users to the database"
+end
+
+Profile.transaction do
+  Profile.connection.execute "INSERT INTO profiles (id, created_at, updated_at, user_id, first_name, last_name, age, location, bio) VALUES #{profiles.join(', ')}"
+  puts "added #{profiles.count} user profiles to the database"
+end
+
+Artist.transaction do
+  Artist.connection.execute "INSERT INTO artists (id, name, created_at, updated_at, spotify_id, photo_url, genres) VALUES #{artists.join(', ')}"
+  puts "added #{artists.count} artists to the database"
+end
+
+Artist.transaction do
+  Artist.connection.execute "INSERT INTO artists_profiles (artist_id, profile_id) VALUES #{artists_profiles.join(', ')}"
+  puts "added #{artists_profiles.count} artists profiles to the database"
+end
+
+InstrumentProfile.transaction do
+  Instrument.connection.execute "INSERT INTO instrument_profiles (profile_id, instrument_id, proficiency, owned, created_at, updated_at) VALUES #{instrument_profiles.join(', ')}"
+  puts "added #{instrument_profiles.count} instrument profiles to the database"
+end
+
+JamGroupMember.transaction do
+  JamGroupMember.connection.execute "INSERT INTO jam_group_members (profile_id, jam_group_id, invited_by, status, created_at, updated_at) VALUES #{members.join(', ')}"
+  puts "added #{members.count} memberships to the database"
+end
+
+Comment.transaction do
+  Comment.connection.execute "INSERT INTO comments (jam_group_id, profile_id, comment, created_at, updated_at) VALUES #{comments.join(', ')}"
+  puts "added #{comments.count} comments to the database"
+end
+
