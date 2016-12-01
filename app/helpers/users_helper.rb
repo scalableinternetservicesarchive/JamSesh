dc4b79d6184630a7fffd8ae9e04671321000b587
Nick Brown <nickbrown714@gmail.com>
2016-12-01 00:45:44 -0800

    adding user list fragment caching

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,12 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.profile.full_name, class: "#{clazz}", size: size)
   end
+
+  def cache_key_for_profile(prof)
+    "profile/#{prof.id}/#{prof.updated_at}/#{prof.instrument.count}"
+  end
+
+  def cache_key_for_profile_listing
+    "profile_table/#{Profile.maximum(:updated_at)}/#{InstrumentProfile.maximum(:updated_at).to_s}"
+  end
 end
