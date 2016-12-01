813097c2a6381d8485657c658ff013e38be8bf66
Nick Brown <nickbrown714@gmail.com>
2016-12-01 00:45:02 -0800

    adding jquery-ui to fix issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ui/autocomplete
+//= require jquery-ui
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-tags-input
